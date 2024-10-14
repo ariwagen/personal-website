a191ae6d1858821faf4c440ee567b4cff9575a63
Arien Wagen <ari.wagen@gmail.com>
2024-10-13 21:44:27 -0400

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,6 @@
 # Navigate to the directory of your app
 cd /home/ec2-user/personal-website || exit
 
-# Pull the latest code from the main branch
-git pull origin main
-
 # Build the React app (if needed)
 npm install
 npm run build
